44029085154942ce4aba7f739b9c1d05fb734adf
David Fitzgibbon <bluetoffeedev@gmail.com>
2014-04-09 15:57:26 +0100

    fix for mongoid 10068 error

--- lib/devise/models/authenticatable.rb
+++ lib/devise/models/authenticatable.rb
@@ -205,7 +205,7 @@ module Devise
         end
 
         def serialize_from_session(key, salt)
-          record = to_adapter.get(key)
+          record = to_adapter.get(key.to_s)  # davyfitz fix for mongoid 10068 error
           record if record && record.authenticatable_salt == salt
         end
 
